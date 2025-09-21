10816e6f41ab1c821d0a2ceb22ef8ad8c9ae81e2
Igor de Almeida <igordealmeida91@gmail.com>
2025-09-21 00:04:32 -0300

    Key press detection using GLFW

--- src/main/java/nesemu/io/GlfwKeyboardController.java
+++ src/main/java/nesemu/io/GlfwKeyboardController.java
@@ -1,9 +1,31 @@
 package nesemu.io;
 
+
+import org.lwjgl.glfw.GLFW;
+
 public class GlfwKeyboardController implements Controller {
 
     @Override
     public boolean checkPressed(ControllerKey key) {
+        switch (key) {
+            case A:
+                return GLFW.glfwGetKey(GLFW.glfwGetCurrentContext(), GLFW.GLFW_KEY_X) == GLFW.GLFW_PRESS;
+            case B:
+                return GLFW.glfwGetKey(GLFW.glfwGetCurrentContext(), GLFW.GLFW_KEY_Z) == GLFW.GLFW_PRESS;
+            case SELECT:
+                return GLFW.glfwGetKey(GLFW.glfwGetCurrentContext(), GLFW.GLFW_KEY_BACKSPACE) == GLFW.GLFW_PRESS;
+            case START:
+                return GLFW.glfwGetKey(GLFW.glfwGetCurrentContext(), GLFW.GLFW_KEY_ENTER) == GLFW.GLFW_PRESS;
+            case UP:
+                return GLFW.glfwGetKey(GLFW.glfwGetCurrentContext(), GLFW.GLFW_KEY_UP) == GLFW.GLFW_PRESS;
+            case DOWN:
+                return GLFW.glfwGetKey(GLFW.glfwGetCurrentContext(), GLFW.GLFW_KEY_DOWN) == GLFW.GLFW_PRESS;
+            case LEFT:
+                return GLFW.glfwGetKey(GLFW.glfwGetCurrentContext(), GLFW.GLFW_KEY_LEFT) == GLFW.GLFW_PRESS;
+            case RIGHT:
+                return GLFW.glfwGetKey(GLFW.glfwGetCurrentContext(), GLFW.GLFW_KEY_RIGHT) == GLFW.GLFW_PRESS;
+        }
+
         return false;
     }
 }
